449666dca68144f48a1641eac11d3997f95b1b19
Sam <samara.amanda01@gmail.com>
2022-04-08 08:07:08 -0400

    users and fitness class routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :fitness_classes
+
+  resources :users do 
+    resources :fitness_classes
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
